b15419f45483eaac6e3aa4df54d122b532dd8755
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-11-26 18:21:24 -0800

    grow snake and partial death

--- index.js
+++ index.js
@@ -1,36 +1,109 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
-function init() {
-    window.requestAnimationFrame(draw);
-}
+var width = 20;
+var height = 20;
+var rate = 100; // draw rate
+var size = Math.floor(canvas.width / width);
 var directions = [
-    { x: -1, y: 0 },
-    { x: 0, y: -1 },
-    { x: 1, y: 0 },
+    { x: -1, y: 0 }, //left
+    { x: 0, y: -1 }, //up
+    { x: 1, y: 0 }, //right
     { x: 0, y: 1 },
 ];
+var score = 0;
 var direction = 0;
-var pos = { x: width / 2, y: height / 2 };
-function draw() {
-    ctx.clearRect(0, 0, 300, 300); // clear canvas
-    window.requestAnimationFrame(draw);
+var parts_pos = [{ x: width / 2, y: height / 2 }];
+growSnake();
+var apple_pos = {
+    x: Math.floor(Math.random() * width),
+    y: Math.floor(Math.random() * height),
+};
+var interval_id = setInterval(draw, rate);
+function objectEqual(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
 }
-init();
+function eatApple() {
+    score++;
+    apple_pos = {
+        x: Math.floor(Math.random() * width),
+        y: Math.floor(Math.random() * height),
+    };
+    console.log(score);
+}
+function growSnake() {
+    var len = parts_pos.length;
+    if (len === 1) {
+        var next_dir_1 = {
+            x: directions[direction].x * -1,
+            y: directions[direction].y * -1,
+        };
+        var next_pos_1 = {
+            x: parts_pos[len - 1].x + next_dir_1.x,
+            y: parts_pos[len - 1].y + next_dir_1.y,
+        };
+        parts_pos.push(next_pos_1);
+        return;
+    }
+    var next_dir = {
+        x: parts_pos[len - 1].x - parts_pos[len - 2].x,
+        y: parts_pos[len - 1].y - parts_pos[len - 2].y,
+    };
+    var next_pos = {
+        x: parts_pos[len - 1].x + next_dir.x,
+        y: parts_pos[len - 1].y + next_dir.y,
+    };
+    parts_pos.push(next_pos);
+}
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    var cur_pos = {
+        x: parts_pos[0].x + directions[direction].x,
+        y: parts_pos[0].y + directions[direction].y,
+    };
+    if (cur_pos.x < 0 ||
+        cur_pos.x >= width ||
+        cur_pos.y < 0 ||
+        cur_pos.y >= height) {
+        console.log("gameover");
+        clearInterval(interval_id);
+    }
+    parts_pos = __spreadArray([cur_pos], parts_pos, true);
+    parts_pos.pop();
+    if (objectEqual(parts_pos[0], apple_pos)) {
+        eatApple();
+        growSnake();
+    }
+    //render snake
+    ctx.fillStyle = "green";
+    parts_pos.forEach(function (pos) {
+        ctx.fillRect(pos.x * size, pos.y * size, size, size);
+    });
+    //render apple
+    ctx.fillStyle = "red";
+    ctx.fillRect(apple_pos.x * size, apple_pos.y * size, size, size);
+}
 window.addEventListener("keydown", function (event) {
-    console.log(event);
-    if (event.key === "ArrowLeft" || event.key === "a") {
-        console.log("left");
+    if ((event.key === "ArrowLeft" || event.key === "a") && direction != 2) {
+        direction = 0;
     }
-    if (event.key === "ArrowUp" || event.key === "w") {
-        console.log("up");
+    if ((event.key === "ArrowUp" || event.key === "w") && direction != 3) {
+        direction = 1;
     }
-    if (event.key === "ArrowDown" || event.key === "s") {
-        console.log("down");
+    if ((event.key === "ArrowRight" || event.key === "d") && direction != 0) {
+        direction = 2;
     }
-    if (event.key === "ArrowRight" || event.key === "d") {
-        console.log("right");
+    if ((event.key === "ArrowDown" || event.key === "s") && direction != 1) {
+        direction = 3;
     }
-    // do something
 });
--- index.ts
+++ index.ts
@@ -1,42 +1,121 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
-const width = canvas.width;
-const height = canvas.height;
 
-function init() {
-    window.requestAnimationFrame(draw);
-}
+const width = 20;
+const height = 20;
+const rate = 100; // draw rate
 
-const directions = [
-    { x: -1, y: 0 }, //left
-    { x: 0, y: -1 }, //up
-    { x: 1, y: 0 }, //right
-    { x: 0, y: 1 }, //down
+const size = Math.floor(canvas.width / width);
+
+type Direction = { x: number; y: number };
+
+const directions: Direction[] = [
+    { x: -1, y: 0 } as Direction, //left
+    { x: 0, y: -1 } as Direction, //up
+    { x: 1, y: 0 } as Direction, //right
+    { x: 0, y: 1 } as Direction, //down
 ];
 
+let score = 0;
 let direction = 0;
-let pos = { x: width / 2, y: height / 2 };
+let parts_pos: Direction[] = [{ x: width / 2, y: height / 2 }];
+growSnake();
 
-function draw() {
-    ctx.clearRect(0, 0, 300, 300); // clear canvas
+let apple_pos = {
+    x: Math.floor(Math.random() * width),
+    y: Math.floor(Math.random() * height),
+} as Direction;
 
-    window.requestAnimationFrame(draw);
+const interval_id = setInterval(draw, rate);
+
+function objectEqual(a: any, b: any) {
+    return JSON.stringify(a) === JSON.stringify(b);
 }
 
-init();
+function eatApple() {
+    score++;
+    apple_pos = {
+        x: Math.floor(Math.random() * width),
+        y: Math.floor(Math.random() * height),
+    };
+    console.log(score);
+}
+
+function growSnake() {
+    const len = parts_pos.length;
+    if (len === 1) {
+        const next_dir = {
+            x: directions[direction].x * -1,
+            y: directions[direction].y * -1,
+        };
+        const next_pos = {
+            x: parts_pos[len - 1].x + next_dir.x,
+            y: parts_pos[len - 1].y + next_dir.y,
+        };
+        parts_pos.push(next_pos);
+        return;
+    }
+    let next_dir: Direction = {
+        x: parts_pos[len - 1].x - parts_pos[len - 2].x,
+        y: parts_pos[len - 1].y - parts_pos[len - 2].y,
+    };
+    const next_pos = {
+        x: parts_pos[len - 1].x + next_dir.x,
+        y: parts_pos[len - 1].y + next_dir.y,
+    };
+    parts_pos.push(next_pos);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let cur_pos = {
+        x: parts_pos[0].x + directions[direction].x,
+        y: parts_pos[0].y + directions[direction].y,
+    };
+
+    if (
+        cur_pos.x < 0 ||
+        cur_pos.x >= width ||
+        cur_pos.y < 0 ||
+        cur_pos.y >= height
+    ) {
+        console.log("gameover");
+        clearInterval(interval_id);
+    }
+
+    parts_pos = [cur_pos, ...parts_pos];
+    parts_pos.pop();
+
+    if (objectEqual(parts_pos[0], apple_pos)) {
+        eatApple();
+        growSnake();
+    }
+
+    //render snake
+    ctx.fillStyle = "green";
+    parts_pos.forEach((pos) => {
+        ctx.fillRect(pos.x * size, pos.y * size, size, size);
+    });
+
+    //render apple
+    ctx.fillStyle = "red";
+    ctx.fillRect(apple_pos.x * size, apple_pos.y * size, size, size);
+}
 
 window.addEventListener("keydown", (event) => {
-    console.log(event);
-    if (event.key === "ArrowLeft" || event.key === "a") {
+    if ((event.key === "ArrowLeft" || event.key === "a") && direction != 2) {
         direction = 0;
     }
-    if (event.key === "ArrowUp" || event.key === "w") {
+    if ((event.key === "ArrowUp" || event.key === "w") && direction != 3) {
         direction = 1;
     }
-    if (event.key === "ArrowRight" || event.key === "d") {
+    if ((event.key === "ArrowRight" || event.key === "d") && direction != 0) {
         direction = 2;
     }
-    if (event.key === "ArrowDown" || event.key === "s") {
+    if ((event.key === "ArrowDown" || event.key === "s") && direction != 1) {
         direction = 3;
     }
 });
