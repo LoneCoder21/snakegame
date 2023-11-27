a29bd2c004e1aca60c0117b14fa63c198f17a850
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-11-26 17:23:31 -0800

    event listeners for controls

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-if (ctx) {
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100);
+var width = canvas.width;
+var height = canvas.height;
+function init() {
+    window.requestAnimationFrame(draw);
 }
+var directions = [
+    { x: -1, y: 0 },
+    { x: 0, y: -1 },
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+];
+var direction = 0;
+var pos = { x: width / 2, y: height / 2 };
+function draw() {
+    ctx.clearRect(0, 0, 300, 300); // clear canvas
+    window.requestAnimationFrame(draw);
+}
+init();
+window.addEventListener("keydown", function (event) {
+    console.log(event);
+    if (event.key === "ArrowLeft" || event.key === "a") {
+        console.log("left");
+    }
+    if (event.key === "ArrowUp" || event.key === "w") {
+        console.log("up");
+    }
+    if (event.key === "ArrowDown" || event.key === "s") {
+        console.log("down");
+    }
+    if (event.key === "ArrowRight" || event.key === "d") {
+        console.log("right");
+    }
+    // do something
+});
--- index.ts
+++ index.ts
@@ -1,7 +1,42 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d")!;
+const width = canvas.width;
+const height = canvas.height;
 
-if (ctx) {
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100);
+function init() {
+    window.requestAnimationFrame(draw);
 }
+
+const directions = [
+    { x: -1, y: 0 }, //left
+    { x: 0, y: -1 }, //up
+    { x: 1, y: 0 }, //right
+    { x: 0, y: 1 }, //down
+];
+
+let direction = 0;
+let pos = { x: width / 2, y: height / 2 };
+
+function draw() {
+    ctx.clearRect(0, 0, 300, 300); // clear canvas
+
+    window.requestAnimationFrame(draw);
+}
+
+init();
+
+window.addEventListener("keydown", (event) => {
+    console.log(event);
+    if (event.key === "ArrowLeft" || event.key === "a") {
+        direction = 0;
+    }
+    if (event.key === "ArrowUp" || event.key === "w") {
+        direction = 1;
+    }
+    if (event.key === "ArrowRight" || event.key === "d") {
+        direction = 2;
+    }
+    if (event.key === "ArrowDown" || event.key === "s") {
+        direction = 3;
+    }
+});
